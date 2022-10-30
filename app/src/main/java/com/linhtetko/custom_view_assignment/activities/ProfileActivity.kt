1f8428066b9083461241f0b82bf9a5d4363ec2e0
Lin Htet Ko <linhtetko1518@gmail.com>
2022-10-30 11:21:19 +0700

    Add New Features

--- app/src/main/java/com/linhtetko/custom_view_assignment/activities/ProfileActivity.kt
+++ app/src/main/java/com/linhtetko/custom_view_assignment/activities/ProfileActivity.kt
@@ -8,12 +8,14 @@ import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.tabs.TabLayoutMediator
 import com.linhtetko.custom_view_assignment.R
 import com.linhtetko.custom_view_assignment.adapters.ProfileTabViewPagerAdapter
+import com.linhtetko.custom_view_assignment.delegate.ProfileActionsDelegate
 import com.linhtetko.custom_view_assignment.mvp.presenters.ProfilePresenter
 import com.linhtetko.custom_view_assignment.mvp.presenters.ProfilePresenterImpl
 import com.linhtetko.custom_view_assignment.mvp.views.ProfileView
+import com.linhtetko.custom_view_assignment.viewpods.ProfileActionsViewPods
 import kotlinx.android.synthetic.main.activity_profile.*
 
-class ProfileActivity : AppCompatActivity(), ProfileView {
+class ProfileActivity : AppCompatActivity(), ProfileView, ProfileActionsDelegate {
 
     companion object {
         fun newIntent(context: Context): Intent {
@@ -29,8 +31,14 @@ class ProfileActivity : AppCompatActivity(), ProfileView {
         setContentView(R.layout.activity_profile)
 
         setUpPresenter()
+        setUpViewPods()
         setUpTabLayout()
         setUpListener()
+    }
+
+    private fun setUpViewPods() {
+        val profileActions = vpProfileActions as ProfileActionsViewPods
+        profileActions.setUpDelegate(this)
     }
 
     private fun setUpListener() {
@@ -58,5 +66,17 @@ class ProfileActivity : AppCompatActivity(), ProfileView {
 
     override fun navigateToBack() {
         super.onBackPressed()
+    }
+
+    override fun onTapShare() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTapMessage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTapDoneTasks() {
+        TODO("Not yet implemented")
     }
 }
